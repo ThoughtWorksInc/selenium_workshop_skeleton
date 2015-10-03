c477d84c0126d3031ee52e73cb2e86fa0a92e146
Guilherme Heuser Prestes <guilherme.prestes@gmail.com>
2015-10-03 11:15:43 -0300

    Changing to Selenium-selenium :)

--- src/test/java/Example.java
+++ src/test/java/Example.java
@@ -2,7 +2,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Example {
 
@@ -10,7 +10,7 @@ public class Example {
 
     @Before
     public void tearUp() {
-        driver = new HtmlUnitDriver();
+        driver = new FirefoxDriver();
     }
 
     @After
